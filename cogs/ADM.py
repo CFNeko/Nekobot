a1661bc430cbfe18b99c1570c936601860ca9620
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2020-03-23 23:48:01 +0100

    all params ready to be inserted

--- cogs/ADM.py
+++ cogs/ADM.py
@@ -9,6 +9,10 @@ BOT_ADMIN_SERVER = os.getenv('BOT_ADMIN_SERVER')
 BOT_ADMIN_CHAN = os.getenv('BOT_ADMIN_CHAN')
 
 
+def _is_admin(ctx):
+    return ctx.author.guild.name == BOT_ADMIN_SERVER and ctx.channel.name == BOT_ADMIN_CHAN
+
+
 class ADM(commands.Cog):
     """Missions Expanded"""
     def __init__(self, bot):
@@ -28,10 +32,23 @@ class ADM(commands.Cog):
             await ctx.send(embed=me_embed.t)
 
     @adm.command()
-    async def test(self, ctx):
-        """should work ONLY if you're admin"""
+    async def me_add(self, ctx, *, args: str):
+        """add a mission entry, but ONLY if you're admin"""
         async with self.bot.db.acquire() as conn:
-            if ctx.author.guild.name == BOT_ADMIN_SERVER and ctx.channel.name == BOT_ADMIN_CHAN:
+            if _is_admin(ctx):
+                params_array = args.split(" ")
+                tag = params_array[0]
+                is_me = params_array[-1]
+                name = " ".join(params_array[1:-1])
+                await ctx.send(f"the tag is {tag}, the name is {name} and is_me is {is_me}")
+            else:
+                await ctx.send("GET OUT, peasant! (but nice try)")
+
+    @adm.command()
+    async def me_suppress(self, ctx):
+        """suppress a mission entry, but ONLY if you're admin"""
+        async with self.bot.db.acquire() as conn:
+            if _is_admin(ctx):
                 await ctx.send("hey, you're admin, amirite")
             else:
                 await ctx.send("GET OUT, peasant! (but nice try)")
