694d5870143df76b610a28b7edd3da3ffc8ab358
CFNeko <55338055+CFNeko@users.noreply.github.com>
2020-06-21 22:00:17 -0400

    Update ME.py

--- cogs/ME.py
+++ cogs/ME.py
@@ -64,8 +64,8 @@ class ME(commands.Cog):
                                f'.php?title=Expanded_Mod_Family/{keyword}')
                 tree = await r.read()
                 soup = BeautifulSoup(tree, 'html.parser')
-                link = soup('a', attrs={'href': re.compile("^/view/missions-expanded-trees/")})
-                print(link)
+                link = soup('a')
+                print(link['href'])
                 # sends idea expanded_data
                 y = ('Tradition', 'Ambition', *x)
                 counter = 0
