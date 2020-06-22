c10eeb31361c4c6070b92639c0487c76bb29163f
CFNeko <55338055+CFNeko@users.noreply.github.com>
2020-06-21 21:57:42 -0400

    data successfully pulled, now cleaning it up

--- cogs/ME.py
+++ cogs/ME.py
@@ -64,7 +64,7 @@ class ME(commands.Cog):
                                f'.php?title=Expanded_Mod_Family/{keyword}')
                 tree = await r.read()
                 soup = BeautifulSoup(tree, 'html.parser')
-                link = soup('a', attrs={'href': re.compile("^http://")})
+                link = soup('a', attrs={'href': re.compile("^/view/missions-expanded-trees/")})
                 print(link)
                 # sends idea expanded_data
                 y = ('Tradition', 'Ambition', *x)
