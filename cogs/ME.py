35cd2be3395dfb59fee77d408c49f96365f43067
CFNeko <55338055+CFNeko@users.noreply.github.com>
2020-06-21 22:31:56 -0400

    Update ME.py

--- cogs/ME.py
+++ cogs/ME.py
@@ -65,7 +65,8 @@ class ME(commands.Cog):
                 tree = await r.read()
                 soup = BeautifulSoup(tree, 'html.parser')
                 for link in soup.find_all('a'):
-                    print(link.get('href'))
+                    if nation in link
+                    print(link)
                 print(link)
                 # sends idea expanded_data
                 y = ('Tradition', 'Ambition', *x)
