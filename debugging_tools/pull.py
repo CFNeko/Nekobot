9d859cc4e39cbfd06103a6fe0c2514d42e28906f
CFNeko <55338055+CFNeko@users.noreply.github.com>
2020-06-23 23:16:31 -0400

    updated NIE ideas, created backup file just in case

--- debugging_tools/pull.py
+++ debugging_tools/pull.py
@@ -9,23 +9,5 @@ import asyncpg
 import asyncio
 
 
-with open('Backup0.txt.json', 'r') as f:
-    fhand = f.read()
-    data = json.loads(fhand)
-#
-# for x, y in data.items():
-#     x = x.replace("_idea_groups_expanded", "").replace("_", " ").title()
-#     print(x)
-#     for a,b in y.items():
-#         b = str(b).replace("{", "").replace("}", "").replace("'", "")
-#         a = a.replace("_", " ").title()
-#         if a == 'Category':
-#             temp = f'     {a}: {b}'
-#
-#             print(temp)
-#         else:
-#             print("     " + a)
-#             print("          " + b )
 
-#
 decode('Backup0.txt', None, None)
