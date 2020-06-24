8a598ecb49e8595dd92157ea59618a0776a5b846
CFNeko <55338055+CFNeko@users.noreply.github.com>
2020-06-23 22:39:13 -0400

    Update MCE.py

--- cogs/MCE.py
+++ cogs/MCE.py
@@ -7,15 +7,15 @@ class MCE(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
-		@commands.group(case_insenstive=True)
-		async def mce(self, ctx):
-			"""Returns MCE steam page"""
-			if ctx.invoked_subcommand is None:
-				mce_embed = embedMaker.embedMaker('Mercenary Companies Expanded', 'https://steamcommunity.com/workshop/filedetails'
-                                                                  '/?id=2125424517', 'Mercenaries at your behest',
-                                                                   0x00fdff,
-                                             					  'Made by Vara & Uber',
-                                             'https://steamuserimages-a.akamaihd.net/ugc/1049849534508701381/B159CC0E84068298D3A785E280AB2DC4EEAACF8F/?imw=268&imh=268&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true' )
-				await ctx.send(embed=mce_embed.t)
+	@commands.group(case_insenstive=True)
+	async def mce(self, ctx):
+		"""Returns MCE steam page"""
+		if ctx.invoked_subcommand is None:
+			mce_embed = embedMaker.embedMaker('Mercenary Companies Expanded', 'https://steamcommunity.com/workshop/filedetails/?id=2125424517', 
+												'Mercenaries at your behest',
+                                                0x00fdff,
+                                         		'Made by Vara & Uber',
+                                         		'https://steamuserimages-a.akamaihd.net/ugc/1049849534508701381/B159CC0E84068298D3A785E280AB2DC4EEAACF8F/?imw=268&imh=268&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true' )
+			await ctx.send(embed=mce_embed.t)
 def setup(bot):
     bot.add_cog(MCE(bot))
