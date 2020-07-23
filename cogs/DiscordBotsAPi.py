47399469fce41062d49c5c34b343272f5a4b9c36
CFNeko <55338055+CFNeko@users.noreply.github.com>
2020-07-23 17:55:20 -0400

    Update DiscordBotsAPi.py

--- cogs/DiscordBotsAPi.py
+++ cogs/DiscordBotsAPi.py
@@ -11,7 +11,7 @@ class TopGG(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.token = 'dbl_token' # set this to your DBL token
+        self.token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMjI4OTg3OTE4Njk5NzI3OCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTk1NTQxMjY0fQ.u_-J8AupS8bJ9JS12i8L2LRhKaFlZArqXAz0sAON5Iw' # set this to your DBL token
         self.dblpy = dbl.DBLClient(self.bot, self.token, webhook_path='/dblwebhook', webhook_auth='password', webhook_port=5000)
 
     # The decorator below will work only on discord.py 1.1.0+
