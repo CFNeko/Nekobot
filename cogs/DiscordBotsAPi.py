5aa24084b16968ca80128a624d829cfeb861f7bc
CFNeko <55338055+CFNeko@users.noreply.github.com>
2020-07-23 18:07:46 -0400

    should be done

--- cogs/DiscordBotsAPi.py
+++ cogs/DiscordBotsAPi.py
@@ -12,14 +12,14 @@ class TopGG(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMjI4OTg3OTE4Njk5NzI3OCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTk1NTQxMjY0fQ.u_-J8AupS8bJ9JS12i8L2LRhKaFlZArqXAz0sAON5Iw' # set this to your DBL token
-        self.dblpy = dbl.DBLClient(self.bot, self.token, webhook_path='/dblwebhook', webhook_auth='password', webhook_port=5000)
+        self.dblpy = dbl.DBLClient(self.bot, self.token)
 
     # The decorator below will work only on discord.py 1.1.0+
     # In case your discord.py version is below that, you can use self.bot.loop.create_task(self.update_stats())
 
-    @tasks.loop(minutes=2.0)
+    @tasks.loop(minutes=30.0)
     async def update_stats(self):
-        """This function runs every 2 minutes to automatically update your server count"""
+        """This function runs every 30 minutes to automatically update your server count"""
         logger.info('Attempting to post server count')
         try:
             await self.dblpy.post_guild_count()
@@ -28,11 +28,6 @@ class TopGG(commands.Cog):
             logger.exception('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
 
         
-
-    @commands.Cog.listener()
-    async def on_dbl_vote(self, data):
-        logger.info('Received an upvote')
-        print(data)
 
 def setup(bot):
     global logger
